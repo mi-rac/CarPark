4d3bf14ccad2ef4ac5faa48d73cb4b2b04229855
Mihai Racoreanu <mihai.racoreanu@gmail.com>
2023-04-13 21:04:17 +0100

    Added CarPark class - linting

--- src/main/java/components/CarPark.java
+++ src/main/java/components/CarPark.java
@@ -1,72 +1,21 @@
 package components;
 
-import java.util.ArrayList;
-import java.util.List;
+public class CarPark
+{
+    private static CarPark instance;
+    private int occupiedSpots;
+    private int maxCapacity;
+    //private List<Sensor> sensors;
+    //private List<IDReader> idReaders;
+    //private FullSign fullSign;
+    //private DataHandler dataHandler;
 
-public class CarPark {
-    private String carParkName;
-    private int carParkCapacity;
-    private double carParkFee;
-    private List<Car> parkedCars;
+    private CarPark() {}
 
-    public CarPark(String carParkName, int carParkCapacity, double carParkFee) {
-        this.carParkName = carParkName;
-        this.carParkCapacity = carParkCapacity;
-        this.carParkFee = carParkFee;
-        parkedCars = new ArrayList<Car>();
-    }
-
-    public boolean parkCar(Car car) {
-        if (parkedCars.size() < carParkCapacity) {
-            parkedCars.add(car);
-            return true;
+    public static CarPark getInstance() {
+        if (instance == null) {
+            instance = new CarPark();
         }
-        return false;
-    }
-
-    public boolean removeCar(Car car) {
-        if (parkedCars.contains(car)) {
-            parkedCars.remove(car);
-            return true;
-        }
-        return false;
-    }
-
-    public double calculateParkingFee(Ticket ticket) {
-        double hoursParked = ticket.getHoursParked();
-        return carParkFee * hoursParked;
-    }
-
-    // Getters and setters
-    public String getCarParkName() {
-        return carParkName;
-    }
-
-    public void setCarParkName(String carParkName) {
-        this.carParkName = carParkName;
-    }
-
-    public int getCarParkCapacity() {
-        return carParkCapacity;
-    }
-
-    public void setCarParkCapacity(int carParkCapacity) {
-        this.carParkCapacity = carParkCapacity;
-    }
-
-    public double getCarParkFee() {
-        return carParkFee;
-    }
-
-    public void setCarParkFee(double carParkFee) {
-        this.carParkFee = carParkFee;
-    }
-
-    public List<Car> getParkedCars() {
-        return parkedCars;
-    }
-
-    public void setParkedCars(List<Car> parkedCars) {
-        this.parkedCars = parkedCars;
+        return instance;
     }
 }
